cdef2f5c9388b1cd782e0e1b81fe09c3f618c5fb
vladoss2899 <vladoss2899@gmail.com>
2024-01-15 22:19:01 +0100

    aa

---
+++ src/js/2-snackbar.js
@@ -0,0 +1,35 @@
+import izitoast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+const formEl = document.querySelector('.form');
+formEl.addEventListener('submit', event => {
+  event.preventDefault();
+  const delay = formEl.elements.delay.value;
+  const stateValue = formEl.elements.state.value;
+  const promis = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (stateValue === 'fulfilled') {
+        resolve(delay);
+      }
+      reject(delay);
+    }, delay);
+  });
+  formEl.reset();
+  promis
+    .then(delay => {
+      izitoast.success({
+        message: `✅ Fulfilled promise in ${delay} ms`,
+        color: 'green',
+        icon: '',
+        position: 'topCenter',
+      });
+    })
+    .catch(delay => {
+      izitoast.error({
+        message: `❌ Rejected promise in ${delay} ms`,
+        color: '#ff0000',
+        icon: '',
+        position: 'topCenter',
+      });
+    })
+    .finally();
+});
