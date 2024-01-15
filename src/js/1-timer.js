1c8742414bc48e169b8b2d15a3c015bcf032fd3b
vladoss2899 <vladoss2899@gmail.com>
2024-01-15 22:40:53 +0100

    aa

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -2,14 +2,17 @@ import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
 import izitoast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
+
 const inputEl = document.querySelector('#datetime-picker');
 const btnEl = document.querySelector('[data-start]');
 const timerDays = document.querySelector('[data-days]');
 const timerHours = document.querySelector('[data-hours]');
 const timerMinutes = document.querySelector('[data-minutes]');
 const timerSeconds = document.querySelector('[data-seconds]');
+
 let userSelectedDate;
 btnEl.disabled = true;
+
 const options = {
   enableTime: true,
   time_24hr: true,
@@ -18,6 +21,7 @@ const options = {
   locale: {
     firstDayOfWeek: 1,
   },
+
   onClose(selectedDates) {
     userSelectedDate = selectedDates[0].getTime();
     if (userSelectedDate < Date.now()) {
@@ -32,32 +36,38 @@ const options = {
     }
   },
 };
+
 flatpickr(inputEl, options);
+
 function convertMs(ms) {
   const second = 1000;
   const minute = second * 60;
   const hour = minute * 60;
   const day = hour * 24;
+
   const days = Math.floor(ms / day);
   const hours = Math.floor((ms % day) / hour);
   const minutes = Math.floor(((ms % day) % hour) / minute);
   const seconds = Math.floor((((ms % day) % hour) % minute) / second);
   return { days, hours, minutes, seconds };
 }
+
 function updateTimer({ days, hours, minutes, seconds }) {
   timerDays.textContent = `${String(days).padStart(2, '0')}`;
   timerHours.textContent = `${String(hours).padStart(2, '0')}`;
   timerMinutes.textContent = `${String(minutes).padStart(2, '0')}`;
   timerSeconds.textContent = `${String(seconds).padStart(2, '0')}`;
 }
+
 btnEl.addEventListener('click', isStartTimer);
 function isStartTimer(event) {
   btnEl.disabled = true;
+
   const intervalId = setInterval(() => {
-    const currentTime = Date.now();
-    const ms = userSelectedDate - currentTime;
-    const restTime = convertMs(ms);
-    updateTimer(restTime);
+    const currentTime = Date.now(); 
+    const ms = userSelectedDate - currentTime; 
+    const restTime = convertMs(ms); 
+    updateTimer(restTime); 
     if (ms <= 1000) {
       clearInterval(intervalId);
     }
--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,10 +1,14 @@
 import izitoast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
+
 const formEl = document.querySelector('.form');
+
 formEl.addEventListener('submit', event => {
   event.preventDefault();
+
   const delay = formEl.elements.delay.value;
   const stateValue = formEl.elements.state.value;
+
   const promis = new Promise((resolve, reject) => {
     setTimeout(() => {
       if (stateValue === 'fulfilled') {
@@ -13,7 +17,9 @@ formEl.addEventListener('submit', event => {
       reject(delay);
     }, delay);
   });
+
   formEl.reset();
+
   promis
     .then(delay => {
       izitoast.success({
